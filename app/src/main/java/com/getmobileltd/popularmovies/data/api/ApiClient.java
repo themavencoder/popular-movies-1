43bc134947856eb175d477203311b0ca934d3e29
Tobiloba Adejumo <eniolaplus08@gmail.com>
2019-04-09 11:24:36 +0100

    feat: Added libraries okhttp and glide to build file

---
+++ app/src/main/java/com/getmobileltd/popularmovies/data/api/ApiClient.java
@@ -0,0 +1,10 @@
+package com.getmobileltd.popularmovies.data.api;
+
+/**
+ * Created by themavencoder on 09,April,2019
+ */
+public class ApiClient {
+    public static final String BASE_URL = "https://api.themoviedb.org/3/";
+
+
+}
---
+++ app/src/main/java/com/getmobileltd/popularmovies/data/model/Movie.java
@@ -0,0 +1,8 @@
+package com.getmobileltd.popularmovies.data.model;
+
+/**
+ * Created by themavencoder on 09,April,2019
+ */
+public class Movie {
+
+}
